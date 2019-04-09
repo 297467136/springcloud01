6f1b913aea2dd8c46fafc3f799454bd97e54bd89
zhouchen <dazhouhuangye@163.com>
2019-04-09 18:30:17 +0800

    springcloud 第一天

---
+++ service-feign/src/main/java/com/example/servicefeign/ServiceFeignApplication.java
@@ -0,0 +1,36 @@
+package com.example.servicefeign;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 消费者
+@EnableFeignClients // 开启 feign
+@EnableCircuitBreaker // 启动断路器
+@EnableHystrixDashboard // Hystrix Dashboard
+public class ServiceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFeignApplication.class, args);
+    }
+
+    // http://localhost:8765/hystrix
+    // http://localhost:8765/hystrix.stream
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
---
+++ service-feign/src/main/java/com/example/servicefeign/common/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.example.servicefeign.common;
+
+import com.example.servicefeign.feign.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
