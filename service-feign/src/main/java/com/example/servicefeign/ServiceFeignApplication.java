007d65646ba50da0939f1eb52d750cb73ad9749c
zhouchen <dazhouhuangye@163.com>
2019-04-10 17:02:59 +0800

    springcloud 第二天

--- service-feign/src/main/java/com/example/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/example/servicefeign/ServiceFeignApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients // 开启 feign
 @EnableCircuitBreaker // 启动断路器
 @EnableHystrixDashboard // Hystrix Dashboard
+@EnableEurekaClient
 public class ServiceFeignApplication {
 
     public static void main(String[] args) {
